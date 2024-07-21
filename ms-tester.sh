4317b6351fab84931e985a5a0780458ab6f31f0f
atormac <atorma@student.hive.fi>
2024-07-21 19:21:42 +0300

    initial commit

---
+++ ms-tester.sh
@@ -0,0 +1,20 @@
+cp ../minishell .
+chmod 755 minishell
+MS=./minishell
+
+PROMPT=$(echo -e "\nexit\n" | $MS | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
+BASH_PROMPT=$(echo -e "\nexit\n" | $MS | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
+
+INPUT="lsasdasd\nexit"
+
+BASH_OUT=$(echo -e "$INPUT" | bash 2>./bash_error 1>./bash_out)
+BASH_EXIT=$?
+echo $BASH_OUT
+echo $BASH_EXIT
+
+MS_OUT=$(echo -e $INPUT | $MS 2>./ms_error | grep -vF "$PROMPT" 1>./ms_out)
+MS_EXIT=$?
+echo $MS_OUT
+echo $MS_EXIT
+
+#exec_test 'echo abc 123'
