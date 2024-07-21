73972867eaab2c9cb1946489a0df5b95ae8fe61b
atormac <atorma@student.hive.fi>
2024-07-21 19:24:45 +0300

    improvements

--- ms-tester.sh
+++ ms-tester.sh
@@ -3,6 +3,7 @@ chmod 755 minishell
 MS=./minishell
 
 PROMPT=$(echo -e "\nexit\n" | $MS | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
+PROMPT_NOENV=$(echo -e "\nexit\n" | env -i $MS | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
 BASH_PROMPT=$(echo -e "\nexit\n" | $MS | head -n 1 | sed "s/\x1B\[[0-9;]\{1,\}[A-Za-z]//g" )
 
 INPUT="lsasdasd\nexit"
@@ -12,7 +13,7 @@ BASH_EXIT=$?
 echo $BASH_OUT
 echo $BASH_EXIT
 
-MS_OUT=$(echo -e $INPUT | $MS 2>./ms_error | grep -vF "$PROMPT" 1>./ms_out)
+MS_OUT=$(echo -e $INPUT | $MS 2>./ms_error | grep -vF "$PROMPT" | grep -vF "$PROMPT_NOENV" 1>./ms_out)
 MS_EXIT=$?
 echo $MS_OUT
 echo $MS_EXIT
