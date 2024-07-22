652cf026a045ce4c1f273b8a9a94f772a209de51
atormac <atorma@proton.me>
2024-07-22 23:33:13 +0300

    Fixes and refactor

--- tester/ms-test.py
+++ tester/ms-test.py
@@ -30,47 +30,21 @@ def get_ms_output(s):
 
 def run_bash(input_str):
     try:
-        b = subprocess.Popen(["bash"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = b.communicate(input_str.encode(), timeout=5)
-        bash_exitcode = b.returncode
-        bash_stdout = stdout.decode().rstrip()
-        bash_stderr = strip_prefix(stderr.decode().rstrip(), "bash: line 1: ")
-        bash_stderr = strip_prefix(bash_stderr, "bash: ")
-        bash_stderr = bash_stderr.partition('\n')[0]
-    except Exception as e:
-        print("Error occured: ", e)
-    return bash_stdout, bash_stderr, bash_exitcode
-
-def run_bash_full(input_str):
-    try:
         line_count = input_str.count('\n')
-        b = subprocess.Popen(["bash"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        b = subprocess.Popen(["bash"], shell=False,
+                stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = b.communicate(input_str.encode(), timeout=5)
         bash_exitcode = b.returncode
         bash_stdout = stdout.decode().rstrip()
         bash_stderr = stderr.decode().rstrip()
+        bash_stderr = strip_prefix(stderr.decode().rstrip(), "bash: line 1: ")
         for x in range(line_count):
             bash_stderr = bash_stderr.replace("bash: line " + str(x) + ": ", "")
+            bash_stderr = bash_stderr.replace("line " + str(x) + ": ", "")
+            bash_stderr = strip_prefix(bash_stderr, "bash: ")
     except Exception as e:
         print("Error occured: ", e)
     return bash_stdout, bash_stderr, bash_exitcode
-
-def run_minishell_full(input_str):
-    try:
-        if (VALGRIND == 1):
-            cmd = valgrind_cmd
-            timeout_seconds = 30
-        else:
-            cmd = ["./minishell"]
-            timeout_seconds = 5
-        b = subprocess.Popen(cmd, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = b.communicate(input_str.encode(), timeout=timeout_seconds)
-        ms_exitcode = b.returncode
-        ms_stdout = get_ms_output(stdout.decode().rstrip())
-        ms_stderr = strip_prefix(stderr.decode().rstrip(), "minishell: ")
-    except Exception as e:
-        print("Error occured: ", e)
-    return ms_stdout, ms_stderr, ms_exitcode
 
 def run_minishell(input_str):
     try:
@@ -83,9 +57,9 @@ def run_minishell(input_str):
         b = subprocess.Popen(cmd, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = b.communicate(input_str.encode(), timeout=timeout_seconds)
         ms_exitcode = b.returncode
-        ms_stdout = get_ms_output(stdout.decode())
-        ms_stderr = strip_prefix(stderr.decode(), "minishell: ")
-        ms_stderr = ms_stderr.partition('\n')[0]
+        ms_stdout = get_ms_output(stdout.decode().rstrip())
+        ms_stderr = strip_prefix(stderr.decode().rstrip(), "minishell: ")
+        ms_stderr = ms_stderr.replace("minishell: ", "");
     except Exception as e:
         print("Error occured: ", e)
     return ms_stdout, ms_stderr, ms_exitcode
@@ -126,9 +100,11 @@ def do_test(input_str):
     os.makedirs(dir)
 
     bash_stdout, bash_stderr, bash_exitcode = run_bash(input_str)
+    bash_stderr = bash_stderr.split('\n')[0]
     if (os.path.exists(dir + "/outfile")):
         os.rename(dir + "/outfile", dir + "/bash_out")
     ms_stdout, ms_stderr, ms_exitcode = run_minishell(input_str)
+    ms_stderr = ms_stderr.partition('\n')[0]
     if (bash_stdout != ms_stdout or bash_stderr != ms_stderr or bash_exitcode != ms_exitcode):
         error = 1
     if (os.path.exists(dir + "/bash_out")):
@@ -158,17 +134,18 @@ def run_tests_complex():
     try:
         with open('complex.txt', 'r') as file:
             data = file.read()
-            b_stdout, b_stderr, b_exit = run_bash_full(data)
-            ms_stdout, ms_stderr, ms_exit = run_bash_full(data)
+            b_stdout, b_stderr, b_exit = run_bash(data)
+            ms_stdout, ms_stderr, ms_exit = run_minishell(data)
             error = 0
             if (b_stdout != ms_stdout or b_stderr != ms_stderr):
                 error = 1
+            
             print_test_result("complex.txt", error, 0, b_stdout, b_stderr, b_exit,
                     ms_stdout, ms_stderr, ms_exit)
     except Exception as e:
         print("Error occured: ", e)
 
-def run_tests():
+def run_tests_single():
     try:
         f = open('./tests.txt', 'r')
         lines = f.readlines()
@@ -180,7 +157,7 @@ def run_tests():
 
 init_tester()
 ms_prompt = get_prompt()
-run_tests()
+run_tests_single()
 run_tests_complex()
 print("\n--- SUMMARY ---")
 print("TOTAL:\t" + str(COUNTER) + "\tOK:\t" + str(OK) + "\tERROR:\t" + str(KO))
