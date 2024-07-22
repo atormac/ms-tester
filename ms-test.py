b0d74c1f65670fe1cb7370261fc136b27f4980ac
atormac <atorma@proton.me>
2024-07-22 13:34:29 +0300

    Tests added and improvements

--- ms-test.py
+++ ms-test.py
@@ -2,10 +2,9 @@ import subprocess
 import os
 import shutil
 
-cmd = ['./minishell']
-bash = ['bash']
-
-COUNTER = 1
+global ms_prompt
+ms_prompt = "empty"
+COUNTER = 0
 OK = 0
 KO = 0
 
@@ -13,29 +12,38 @@ def strip_prefix(s, p):
    return s.replace(p,'') if s.startswith(p) else s
 
 def get_ms_output(s):
-    output = ""
-    output = '\n'.join(line for line in s.splitlines() if '@minishell:' not in line)
-    return output
+    global ms_prompt
+    lines = s.splitlines()
+    filtered = [line for line in lines if not line.startswith(ms_prompt)]
+    return '\n'.join(filtered)
 
 def run_bash(input_str):
-    b = subprocess.Popen(["bash"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = b.communicate(input_str.encode())
-    bash_exitcode = b.returncode
-    bash_stdout = stdout.decode().rstrip()
-    bash_stderr = strip_prefix(stderr.decode(), "bash: line 1: ")
-    bash_stderr = strip_prefix(bash_stderr, "bash: ")
+    try:
+        b = subprocess.Popen(["bash"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = b.communicate(input_str.encode(), timeout=5)
+        bash_exitcode = b.returncode
+        bash_stdout = stdout.decode().rstrip()
+        bash_stderr = strip_prefix(stderr.decode(), "bash: line 1: ")
+        bash_stderr = strip_prefix(bash_stderr, "bash: ")
+    except Exception as e:
+        print("Error occured: ", e)
     return bash_stdout, bash_stderr, bash_exitcode
 
 def run_minishell(input_str):
-    b = subprocess.Popen(["./minishell"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = b.communicate(input_str.encode())
-    ms_exitcode = b.returncode
-    ms_stdout = get_ms_output(stdout.decode())
-    ms_stderr = strip_prefix(stderr.decode(), "minishell: ")
+    try:
+        b = subprocess.Popen(["./minishell"], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = b.communicate(input_str.encode(), timeout=5)
+        ms_exitcode = b.returncode
+        ms_stdout = get_ms_output(stdout.decode())
+        ms_stderr = strip_prefix(stderr.decode(), "minishell: ")
+    except Exception as e:
+        print("Error occured: ", e)
     return ms_stdout, ms_stderr, ms_exitcode
 
 def do_test(input_str):
     global COUNTER
+    global OK
+    global KO
 
     error = 0
     bash_stdout, bash_stderr, bash_exitcode = run_bash(input_str)
@@ -54,15 +62,20 @@ def do_test(input_str):
         error = 1
         print("EXITCODE: bash: " + str(bash_exitcode) + "ms: " + str(ms_exitcode))
     if (error == 1):
+        KO += 1
         print("ERROR [" + str(COUNTER) + "]: " + repr(input_str))
     else:
+        OK += 1
         print("OK [" + str(COUNTER) + "]: " + repr(input_str))
     COUNTER += 1
 
 def init_tester():
     global COUNTER
-    COUNTER = 1
     shutil.copyfile("../minishell", "./minishell")
+
+def get_prompt():
+    stdout, stderr, ret = run_minishell("exit\n")
+    return stdout.split(':')[0] + ":"
 
 def run_tests():
     try:
@@ -71,8 +84,11 @@ def run_tests():
         for line in lines:
             do_test(line)
         f.close()
+        print("\n--- SUMMARY ---")
+        print("TOTAL:\t" + str(COUNTER) + "\tOK:\t" + str(OK) + "\tERROR:\t" + str(KO))
     except Exception as e:
         print("Error occured: ", e)
 
 init_tester()
+ms_prompt = get_prompt()
 run_tests()
